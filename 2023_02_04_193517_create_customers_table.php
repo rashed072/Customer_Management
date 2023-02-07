a1677a65710e27a6422057401597549d1541a2f8
Md. Rashedul Islam <120312969+rashed072@users.noreply.github.com>
2023-02-07 15:33:21 +0600

    Add files via upload

---
+++ 2023_02_04_193517_create_customers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id('id');
+            $table->string('user_name',100);
+            $table->string('email');
+            $table->enum('gender',["M","F","O"])->nullable();
+            $table->text('address');
+            $table->date('dob');
+            $table->string('password');
+            $table->boolean('status')->default(1);
+            $table->integer('points')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+};
